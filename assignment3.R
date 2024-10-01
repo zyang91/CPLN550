6e10a067bebcba9139f706b38b1172cbda23fd82
zyang91 <zhanchao@upenn.edu>
2024-10-01 17:51:31 -0400

    updated

--- assignment3.R
+++ assignment3.R
@@ -16,6 +16,8 @@ sort(unique(hh$HH_ID))[sample(11:20,1)]
 random <- hh %>%
   filter(HH_ID == 101162)
 
+random_person <- per %>%
+  filter(HH_ID == 101162)
 # Question 2
 random_trip <- trip %>%
   filter(HH_ID == 101162)
@@ -26,19 +28,23 @@ bike_trip <- trip %>%
 bike_trip %>%
   summarise(total_trips=sum(P_WEIGHT, na.rm=TRUE))
 
-#Question 4
-O_trip <- trip %>%
-  filter(O_COUNTY== random$H_COUNTY)
-O_trip_mode <- O_trip %>%
+# Question 4
+## join the household and trip data
+complet_trip <-left_join(hh, trip, by = "HH_ID")
+
+H_trip <- complet_trip %>%
+  filter(H_COUNTY== random$H_COUNTY)
+
+H_trip_mode <- H_trip %>%
   filter(!is.na(MODE_AGG)) %>%
   group_by(MODE_AGG) %>%
   summarise(total_trips_by_mode=sum(P_WEIGHT, na.rm=TRUE))
 
-O_tripPercentage <- O_trip_mode %>%
-  mutate(percentage = total_trips_by_mode/sum(total_trips_by_mode)*100) %>%
-  arrange(desc(percentage))
+H_tripPercentage <- H_trip_mode %>%
+  mutate(percentage = total_trips_by_mode/sum(total_trips_by_mode)*100)
 
-#Question 5
+
+# Question 5
 household <- left_join(hh, veh, by = "HH_ID")
  ## clean the data-removing income missing values
 household <- household %>%
@@ -51,8 +57,8 @@ household <- household %>%
   filter(!YEAR %in% c(9998, 9999))%>%
   mutate(VEHAGE= 2014- YEAR)
 
-#plot boxplot
-## Converted the income number into text
+## plot boxplot
+### Converted the income number into text
 household <-household %>%
   mutate(income= case_when(INCOME==1 ~ "Under $10,000",
                            INCOME==2 ~ "$10,000-$24,999",
@@ -64,17 +70,30 @@ household <-household %>%
                            INCOME==8 ~ "$150,000-$199,999",
                            INCOME==9 ~ "$200,000-$249,999",
                            INCOME==10 ~ "$250,000 or more"))
-household$income <- factor(household$income, levels = c("Under $10,000", "$10,000-$24,999", "$25,000-$34,999",
-                                              "$35,000-$49,999", "$50,000-$74,999", "$75,000-$99,999",
-                                              "$100,000-$149,999", "$150,000-$199,999", "$200,000-$249,999", 
-                                              "$250,000 or more"))
+household$income <- factor(household$income, levels = c("Under $10,000", 
+                                                        "$10,000-$24,999", 
+                                                        "$25,000-$34,999",
+                                                        "$35,000-$49,999", 
+                                                        "$50,000-$74,999", 
+                                                        "$75,000-$99,999",
+                                                        "$100,000-$149,999", 
+                                                        "$150,000-$199,999", 
+                                                        "$200,000-$249,999", 
+                                                        "$250,000 or more"))
 ggplot(household, aes(x= income, y= VEHAGE))+
   geom_boxplot()+
   labs(title="Boxplot of Car Age by Household Income Level", 
        x="Income level", y= "Vehicle Age")
 
+### Without out lier plot
+ggplot(household, aes(x = income, y = VEHAGE)) +
+  geom_boxplot(outlier.shape = NA) +
+  labs(
+    title = "Boxplot of Car Age by Household Income Level",
+    x = "Income level", y = "Vehicle Age")+
+  ylim(0, 30)
 
-#Question 6
+# Question 6
 modechoice<-left_join(hh, trip, by = "HH_ID")
 
 ## clean the data-removing income missing values
@@ -88,22 +107,24 @@ modechoice <- modechoice %>%
 modechoice <- modechoice %>%
   mutate(mode= case_when(MODE_AGG==1 ~ "Walk",
                              MODE_AGG==2 ~ "Bike",
-                             MODE_AGG==3 ~ "Pravite Vehicle",
-                             MODE_AGG==4 ~ "Pravite Transit",
+                             MODE_AGG==3 ~ "Private Vehicle",
+                             MODE_AGG==4 ~ "Private Transit",
                              MODE_AGG==5 ~ "Public Transit",
                              MODE_AGG==6 ~ "School Bus",
                              MODE_AGG==7 ~ "Other"))
 
 modechoice$mode <- factor(modechoice$mode, levels = c("Walk", 
                                                       "Bike", 
-                                                      "Pravite Vehicle", 
-                                                      "Pravite Transit", 
+                                                      "Private Vehicle", 
+                                                      "Private Transit", 
                                                       "Public Transit", 
                                                       "School Bus", 
                                                       "Other"))
+## plot boxplot
 ggplot(modechoice, aes(x= mode, y= INCOME, fill = mode))+
   geom_boxplot()+
   labs(title="Boxplot of Household Income by Mode Choice", 
        x="Mode Choice", y= "Household Income")+
   theme(legend.position = "none")
+
 
