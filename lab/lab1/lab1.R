e9e9a8133c076ea9d98c47fde37499e45ab2b8e7
zyang91 <zhanchao@upenn.edu>
2024-10-24 11:23:04 -0400

    reorganize and lab3

---
+++ lab/lab1/lab1.R
@@ -0,0 +1,47 @@
+# setting work directory
+setwd("C:/Users/zyang/OneDrive/文档/data")
+
+
+# import the data
+hh<- read.csv("1_Household_Public.csv")
+per <- read.csv("2_Person_Public.csv")
+veh <- read.csv("3_Vehicle_Public.csv")
+trip <- read.csv("4_Trip_Public.csv")
+
+# require packages
+library(ggplot2)
+library(dplyr)
+
+# Question 1
+head(trip)
+subset(trip[c(2,3,4,15,28:38)], PERSON_ID== 10014001)
+
+# Question 2
+mode_share <- trip %>%
+  filter(!is.na(MODE_AGG))%>%
+  group_by(MODE_AGG)%>%
+  summarise(MODE_AGG = n())%>%
+  mutate(percentage = MODE_AGG/sum(MODE_AGG)*100)
+
+
+# Question 3
+test <- merge(trip,hh, by = "HH_ID")
+
+table(test$MODE_AGG[test$H_COUNTY ==  42101]) 
+
+round(table(trip$MODE_AGG[trip$H_COUNTY == 42101])/sum(table(trip$MODE_AGG[trip$H_COUNTY == 42101])), 3)
+
+Philadelphia <- left_join(trip, hh, by = "HH_ID")
+Philadelphia <- Philadelphia %>%
+  filter(H_COUNTY==42101)
+Phildelphia <- Philadelphia %>%
+  group_by(MODE_AGG)%>%
+  summarise(MODE_AGG = n())%>%
+  mutate(percentage = MODE_AGG/sum(MODE_AGG)*100)
+
+#Question 4
+library(doBy)
+weighted.mean(trip$MODE_AGG==1, trip$P_WEIGHT, na.rm = TRUE)
+weighted.mean(trip$MODE_AGG==2, trip$P_WEIGHT, na.rm = TRUE)
+weighted.mean(trip$MODE_AGG==3, trip$P_WEIGHT, na.rm = TRUE)
+weighted.mean(trip$MODE_AGG==4, trip$P_WEIGHT, na.rm = TRUE)
---
+++ lab/lab3/3.4 choice rider.R
@@ -0,0 +1,68 @@
+# Load Rda file from data_processing and fit models
+rm(list=ls())
+
+library (stargazer)
+
+load("choice_rider.rda")
+
+summary(trip_train$p_transit)
+
+plot(I(trip_train$Transit_JourneyTime-trip_train$Auto_TravelTime), trip_train$p_transit,)
+
+
+logit0 <- glm(p_transit ~ 1, data = trip_train,family = binomial(link="logit"))
+
+logit1 <- glm(p_transit ~     I(Transit_JourneyTime-Auto_TravelTime) +
+                I(Transit_Fare- AutoCost),
+              data = trip_train,family = binomial(link="logit"))
+
+summary(logit1)
+
+1- logLik(logit1)/logLik(logit0)
+
+logit1$coefficients
+
+exp(logit1$coefficients)
+
+
+logit2 <- glm(p_transit ~ OP_VEH + 
+                GEND2 + AGECAT2 + INCOME2 + RACE2 + EDUCA2 +has_under5 +
+                I(Transit_JourneyTime-Auto_TravelTime) +
+                I(Transit_Fare- AutoCost) 
+              ,
+              data = trip_train,family = binomial(link="logit"))
+
+
+logit3 <- glm(p_transit ~ OP_VEH + 
+                GEND2 + AGECAT2 + INCOME2 + RACE2 + EDUCA2 +has_under5
+              + LandUseIndex +
+                DistToCBD + PARKCOST_METER + 
+                I(Transit_JourneyTime-Auto_TravelTime) +
+                I(Transit_Fare- AutoCost) 
+              + busFrqAvg + rail_800m,
+              data = trip_train,family = binomial(link="logit"))
+
+
+stargazer(logit1, logit2, logit3, type ="text")
+stargazer(logit1, logit2, logit3, title= "Regression comparision", type="html", 
+          align=TRUE,out="model_comparison_010621.html")
+
+
+
+
+####Probability predictions
+trip_train$prob <- predict(logit1, type = "response")
+
+summary(trip_train$prob)
+
+
+hist(trip_train$prob)
+
+table(trip_train$prob >= .4 & trip_train$prob <= .6)
+
+
+trip_test$prob <- predict(logit1, type = "response", newdata=trip_test)
+
+boxplot(trip_test$prob ~ trip_test$p_transit)
+
+
